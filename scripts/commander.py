83487a3de7fcd89b55d81c2e729f685129a4db02
nedlrichards <edwardlrichards@gmail.com>
2018-05-24 15:44:49 -0700

    commander is publishing user defined message

--- scripts/commander.py
+++ scripts/commander.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 # basic ropy tutorial
 import rospy
-from std_msgs.msg import String
-from zoidberg_nav import DepthChange
+from std_msgs.msg import String, Header
+from zoidberg_nav.msg import DepthChange
 
 def commander():
-    pub = rospy.Publisher('zoidberg/String', String, queue_size=10)
-    rospy.init_node('commander', anonymous=True)
-    rate = rospy.Rate(1) # 10hz
+    pubdc = rospy.Publisher('zoidberg/DepthChange', DepthChange, queue_size=10)
+    rospy.init_node('commander', anonymous=False)
+    rate = rospy.Rate(10) # 10hz
+    publish_time = 0.
+    publish_duration = 5.
+    targetdepth = 0.5
     while not rospy.is_shutdown():
-        hello_str = "time is %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        curr_time = rospy.get_time()
+        if publish_time + publish_duration < curr_time:
+		h = Header(stamp=rospy.Time.now())
+                publish_time = curr_time
+                Timeout = rospy.Duration(secs=publish_duration)
+                dc = DepthChange(header=h, start_time=publish_time, timeout=Timeout,
+                                 target_depth=targetdepth)
+                rospy.loginfo(dc)
+                pubdc.publish(dc)
         rate.sleep()
 
 if __name__ == '__main__':
