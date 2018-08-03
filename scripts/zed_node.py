591f442e2d8e4560558784324a9a19d9b1dc9dbf
SDCR <sdcrobotics@gmail.com>
2018-08-03 09:20:46 -0700

    zednode

--- scripts/zed_node.py
+++ scripts/zed_node.py
@@ -5,11 +5,7 @@ from cv_bridge import CvBridge
 
 class ZedListener:
     def __init__(self):
-<<<<<<< Updated upstream
         self.bridge = CvBridge()
-=======
-        pass
->>>>>>> Stashed changes
 
     def callback(self, data):
         cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -19,35 +15,7 @@ class ZedListener:
         rospy.Subscriber("zed/rgb/image_raw_color", Image, self.callback)
         rospy.spinOnce()
 
-<<<<<<< Updated upstream
     def getImage(self):
         self.listener()
         return self.image
-=======
-        # In ROS, nodes are uniquely named. If two nodes with the same
-        # node are launched, the previous one is kicked off. The
-        # anonymous=True flag means that rospy will choose a unique
-        # name for our 'listener' node so that multiple listeners can
-        # run simultaneously.
-        rospy.init_node('listener', anonymous=True)
 
-        rospy.Subscriber("zed/rgb/image_raw_color", Image, callback)
-
-        # spin() simply keeps python from exiting until this node is stopped
-        rospy.spin()
-
-    def talker(self, data):
-        pub = rospy.Publisher('objectCoordinates', String, queue_size=10)
-        rospy.init_node('talker', anonymous=True)
-        rate = rospy.Rate(10) # 10hz
-        while not rospy.is_shutdown():
-            rospy.loginfo(data)
-            pub.publish(data)
-            rate.sleep()
-
-if __name__ == '__main__':
-    try:
-        listener()
-    except rospy.ROSInterruptException:
-        pass
->>>>>>> Stashed changes
